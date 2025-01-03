050cf751e497eaa4e0ad69e132fe6e0b026f3c49
BasantJoshi6363 <basantjoshi6363@gmail.com>
2025-01-03 06:15:10 +0545

    router dom added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Home from './pages/home/Home'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   )
 }
